73cbf097c765c91652adfda85a97454cb0e782d3
RishabhPurwar24 <rishabh15772@gmail.com>
2021-12-24 23:50:49 +0530

    setup project

---
+++ src/components/Heading.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+
+const Heading = () => {
+  return (
+    <div>
+      <h1 className="heading">devfinder</h1>
+      <button>LIGHT</button>
+    </div>
+  );
+};
+
+export default Heading;
