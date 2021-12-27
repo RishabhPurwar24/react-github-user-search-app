ce8d77d9669de7754e3f2b3c970b693547517cd6
RishabhPurwar24 <rishabh15772@gmail.com>
2021-12-27 23:08:51 +0530

    styling change

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Card from "./components/Card";
 import Header from "./components/Header";
 import Search from "./components/Search";
+import useLocalStorage from "use-local-storage";
 
 function App() {
   const [userData, setUserData] = useState({});
@@ -10,9 +11,22 @@ function App() {
     setUserData(data);
   };
 
+  // const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const defaultDark = "dark";
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
+  console.log(theme);
+  const switchThemeHandler = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    console.log(theme);
+  };
+
   return (
-    <div className="container">
-      <Header />
+    <div className="container" data-theme={theme}>
+      <Header theme={theme} switchThemeHandler={switchThemeHandler} />
       <Search resultData={resultData} />
       <Card userData={userData} />
     </div>
