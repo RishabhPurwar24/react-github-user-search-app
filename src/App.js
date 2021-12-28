1969cad7e3e8ad5c61efaa4355be30ce9b603e92
RishabhPurwar24 <rishabh15772@gmail.com>
2021-12-28 12:41:43 +0530

    feat: add dark & light mode and opacity

--- src/App.js
+++ src/App.js
@@ -7,29 +7,50 @@ import useLocalStorage from "use-local-storage";
 
 function App() {
   const [userData, setUserData] = useState({});
+  const [darkMode, setDarkMode] = useState(true);
   const resultData = (data) => {
     setUserData(data);
   };
 
   // const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-  const defaultDark = "dark";
-  const [theme, setTheme] = useLocalStorage(
-    "theme",
-    defaultDark ? "dark" : "light"
-  );
-  console.log(theme);
-  const switchThemeHandler = () => {
-    const newTheme = theme === "light" ? "dark" : "light";
-    setTheme(newTheme);
-    console.log(theme);
+  // const defaultDark = "dark";
+  // const [theme, setTheme] = useLocalStorage(
+  //   "theme",
+  //   defaultDark ? "dark" : "light"
+  // );
+  // console.log(theme);
+  // const switchThemeHandler = () => {
+  //   const newTheme = theme === "light" ? "dark" : "light";
+  //   setTheme(newTheme);
+  //   console.log(theme);
+  // };
+
+  const modeHandler = (value) => {
+    if (value) {
+      document.body.classList.add("dark-mode");
+      document.body.classList.remove("light-mode");
+    } else {
+      document.body.classList.add("light-mode");
+      document.body.classList.remove("dark-mode");
+    }
+    console.log(value);
+    setDarkMode(!darkMode);
   };
 
   return (
-    <div className="container" data-theme={theme}>
-      <Header theme={theme} switchThemeHandler={switchThemeHandler} />
-      <Search resultData={resultData} />
-      <Card userData={userData} />
+    <div>
+      <div className="container">
+        <Header modeHandler={modeHandler} darkMode={darkMode} />
+        <Search resultData={resultData} />
+        <Card userData={userData} />
+      </div>
     </div>
+
+    // <div className="container" data-theme={theme}>
+    //   <Header theme={theme} switchThemeHandler={switchThemeHandler} />
+    //   <Search resultData={resultData} />
+    //   <Card userData={userData} />
+    // </div>
   );
 }
 
